b524d294afa53ec722483cccf0836e11ad559f16
unknown <damir777xolnazarov@gmail.com>
2023-03-28 21:14:36 +0500

    overall

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ let minute = document.querySelector('#se')
 
 second.innerHTML = new Date().getSeconds()
 minute.innerHTML = new Date().getMinutes()
+let input1 = document.querySelector('#in1')
+let input2 = document.querySelector('#in2')
+input1.oninput = () =>{
+    input2.value = input1.value * 31.2
+}
+input2.oninput = () =>{
+    input1.value = input2.value / 31.2
+}
